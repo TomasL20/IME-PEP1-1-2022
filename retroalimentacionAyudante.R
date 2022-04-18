37b0bea39d8ded923894d3ac2675d0ef45f3d6d8
TomL20 <tomas.lopez.a@usach.cl>
2022-04-18 10:52:04 -0400

    Soluciones de ejemplos profe
    scripts de los EP solucionados por el profesor dado como ejemplo de buena respuesta y script con los comentarios de la ayudante sobre nuestros trabajos prácticos como retroalimentación

---
+++ retroalimentacionAyudante.R
@@ -0,0 +1,18 @@
+#observaciones y cosas que les faltaba a algunos grupos:
+  
+  #Errores frecuentes y observaciones:
+  #No olvidar definir las hipótesis nula y alternativa en lenguaje tanto natural como matemático.
+
+#Fijarse bien si la prueba es unilateral o bilateral. Cuando en el enunciado menciona algo como 'El Comité Olímpico cree que el mejor tiempo medio de los atletas blancos después de ingresar al programa de entrenamiento es superior a 11,78 segundos.', sería unilateral. Pero si dice algo como 'El Comité Olímpico cree que el mejor tiempo medio de los atletas blancos después de ingresar al programa de entrenamiento es de 13,57 segundos.', la prueba sería bilateral.
+
+#Mencionar qué prueba van a utilizar.
+#Ejemplo: Se piensa utilizar la prueba t de Student para una muestra, dado que se solicita inferir acerca de la media del mejor tiempo de los atletas blancos luego de ingresar al programa.
+
+#Verificar el cumplimiento de las condiciones para utilizar la prueba que se propone.
+#Ejemplo: Antes de llevar a cabo la prueba t de Student para una muestra, es necesario verificar el cumplimiento de las condiciones.
+#Lo ideal es utilizar gráficos o el test de Shapiro para verificar normalidad.
+#Mencionar si las observaciones son o no independientes.
+
+#Cuando se solicita el tamaño de la muestra, es necesario redondear la cifra, utilizando la función "ceiling" de R.
+
+#Recordar que cuando p-valor > alpha, se falla al rechazar la hipótesis nula. Y cuando p-valor < alpha, se rechaza la hipótesis nula.
